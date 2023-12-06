130d4855aff74ac9bb3da6ad9474d02a8c9c3fda
tickflag <tickflag-developer@yandex.ru>
2023-12-06 18:31:54 +0300

    DetectFall.ipynb

--- UseYolo.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-from settings import settings
-
-
-model_pose = YOLO('yolov8s-pose.pt')
-
-def yolo_pose(source):
-    results = model_pose.predict(source=source, save=True, conf=0.3, vid_stride=1, show=True, device=0)
-    return results
--- settings.py
+++
@@ -1,5 +0,0 @@
-settings = {
-    'device': '0',
-    'source': 'datasets/our-images/03.mp4',
-    'pose_model': 'yolov8s-pose.pt'
-}
